e07554e5bf58bd1b151328a82bbd6679163e96dc
bealorf <142838950+rebreze@users.noreply.github.com>
2025-09-02 21:01:55 +0300

    request.cmd

--- request.cmd
+++ request.cmd
@@ -1,5 +1,14 @@
 @echo off
+
+echo ==============================
+echo Установка библиотеки requests
+echo ==============================
 python -m pip install --user requests
-python cat_requests.py
+
+echo.
+echo ------------------------------
+echo Запуск скрипта cat_facts.py
+echo ------------------------------
+python cat_facts.py
 
 pause
